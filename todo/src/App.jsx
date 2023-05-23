674abbfd3310c4eb4a33dde744a15d9cf75975da
Shakthi Ganesh S <shakthi.ganesh@kyndryl.com>
2023-05-23 21:43:51 +0530

    Pushing to rep for the first time

---
+++ todo/src/App.jsx
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+
+function App() {
+  
+
+  return<div> <div className="title-box">
+    <h1>TODO </h1></div>
+    <div className="list-box"></div>
+    <div className="entry-box">
+      <form>
+        <label for="todo">Type something to do</label><br></br>
+    <input className="input-box" type="text" placeholder="Enter the task to add"></input>
+      </form>
+    </div>
+  </div>
+ 
+  
+    
+}
+
+
+export default App;
